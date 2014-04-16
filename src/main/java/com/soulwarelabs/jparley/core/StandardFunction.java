e31b3f437161af051981c485d670aec5c3ba0b88
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-04-16 15:27:57 +0400

    StandardFunction API updated

--- src/main/java/com/soulwarelabs/jparley/core/StandardFunction.java
+++ src/main/java/com/soulwarelabs/jparley/core/StandardFunction.java
@@ -4,7 +4,7 @@
  *
  * File:     StandardFunction.java
  * Folder:   /.../com/soulwarelabs/jparley/core
- * Revision: 1.01, 16 April 2014
+ * Revision: 1.02, 16 April 2014
  * Created:  16 March 2014
  * Author:   Ilya Gubarev
  *
@@ -26,6 +26,7 @@
 package com.soulwarelabs.jparley.core;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.soulwarelabs.jcommons.Box;
 
@@ -46,40 +47,125 @@ import com.soulwarelabs.jparley.Function;
 public class StandardFunction extends StandardSubroutine implements Function {
 
     private int counter;
-    private Converter resultDecoder;
-    private Object resultKey;
-    private String structStruct;
-    private int resultType;
-    private Box<Object> resultValue;
+    private Converter decoder;
+    private int index;
+    private String struct;
+    private int type;
+    private Box<Object> value;
 
     /**
      * Creates a new instance of standard function.
      *
      * @param name function name.
+     * @param type function result SQL type code.
      *
      * @since v1.0
      */
-    public StandardFunction(String name, int resultType) {
-        this(name, resultType, null, null);
+    public StandardFunction(String name, int type) {
+        this(name, type, null, null);
     }
 
-    public StandardFunction(String name, int resultType,
-            Interceptor preInterceptor, Interceptor postInterceptor) {
+    /**
+     * Creates a new instance of standard function.
+     *
+     * @param name function name.
+     * @param type function result SQL type code.
+     * @param preInterceptor SQL pre-execution interceptor (optional).
+     * @param postInterceptor SQL post-execution interceptor (optional).
+     *
+     * @see Interceptor
+     *
+     * @since v1.0
+     */
+    public StandardFunction(String name, int type, Interceptor preInterceptor,
+            Interceptor postInterceptor) {
         super(name, preInterceptor, postInterceptor);
-        this.resultKey = 1;
-        this.resultType = resultType;
-        this.resultValue = new Box<Object>();
+        this.index = 1;
+        this.type = type;
+        this.value = new Box<Object>();
         resetCounter();
+    }
+
+    /**
+     * Gets function result SQL data decoder (optional).
+     *
+     * @return SQL data decoder (optional).
+     *
+     * @see Converter
+     *
+     * @since v1.0
+     */
+    public Converter getDecoder() {
+        return decoder;
+    }
+
+    /**
+     * Sets function result SQL data decoder (optional).
+     *
+     * @param decoder SQL data decoder (optional).
+     *
+     * @see Converter
+     *
+     * @since v1.0
+     */
+    public void setDecoder(Converter decoder) {
+        this.decoder = decoder;
     }
 
     @Override
     public Object getResult() {
-        return resultValue.getValue();
+        return value.getValue();
+    }
+
+    /**
+     * Gets function result SQL structure name.
+     *
+     * @return SQL structure name.
+     *
+     * @since v1.0
+     */
+    public String getStruct() {
+        return struct;
+    }
+
+    /**
+     * Sets function result SQL structure name.
+     *
+     * @param struct SQL structure name.
+     *
+     * @since v1.0
+     */
+    public void setStruct(String struct) {
+        this.struct = struct;
+    }
+
+    /**
+     * Gets function result SQL type code.
+     *
+     * @return SQL type code.
+     *
+     * @since v1.0
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * Sets function result SQL type code.
+     *
+     * @param type SQL type code.
+     *
+     * @since v1.0
+     */
+    public void setType(int type) {
+        this.type = type;
     }
 
     @Override
-    public void execute(Connection connection) {
-        output(resultKey, resultType, structStruct, resultDecoder);
+    public void execute(Connection connection) throws SQLException {
+        output(index, getType(), getStruct(), getDecoder());
+        super.execute(connection);
+        // TODO: unregister function result...
     }
 
     @Override
@@ -103,6 +189,6 @@ public class StandardFunction extends StandardSubroutine implements Function {
     }
 
     private void resetCounter() {
-        counter = 2;
+        counter = index + 1;
     }
 }
